d61b6babb26ae0cb07eff0c78e48f15b0a7a87c5
MPE <60610152+EC-IT@users.noreply.github.com>
2020-02-03 14:52:28 +0100

    Create Get-ADGroupMemberDisable.ps1
    Gets users disabled in an Active Directory group.

---
+++ Get-ADGroupMemberDisable.ps1
@@ -0,0 +1,28 @@
+﻿
+# liste les membre Disable
+function Get-ADGroupMemberDisable()
+{
+    param($groupname, $domain)
+
+    # recupere la liste des memebres
+    $Group = Get-ADGroup -Filter "Name -eq '$groupname'" -Server $domain -Properties member
+
+    # pour chaque memebre...
+    foreach ( $user in $Group.member )
+    {
+        # si il n'est pas dans un autre domaine
+        if ( $user -notlike "*ForeignSecurityPrincipals*" )
+        {        
+            # recupere le status enable
+            $actif = $(Get-ADUser -Identity "$user" -Server $domain -Properties * -ErrorAction SilentlyContinue).Enabled
+
+            # si enable a False alors affiche son nom
+            if ( $actif -eq "False" )
+            {
+                Write-Output $user
+            }
+        }
+    }
+
+}
+
