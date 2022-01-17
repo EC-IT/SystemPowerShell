941330b78074c1181075c24ab0fe2587960270d5
EC-IT <60610152+EC-IT@users.noreply.github.com>
2022-01-17 11:43:05 +0100

    hyper-v event 15 solved

---
+++ Hyper-V_Removed_orphaned_port_vmswitch.ps1
@@ -0,0 +1,23 @@
+﻿# MPE - JAN 2022 - Removed Orphaned PortID
+# source : http://www.kuskaya.info/2020/04/19/how-to-troubleshoot-event-id-15-from-source-hyper-v-vmswitch-failed-to-restore-configuration-for-port-xxx-on-switch-xxx-object-name-not-found/
+
+$VMSwitchExtensionPortData = Get-VMSwitchExtensionPortData -VMName *
+$switchid = $(Get-VMSwitch)[0].Id.Guid
+$vmswitch_keys = Get-ChildItem -Path HKLM:\SYSTEM\CurrentControlSet\Services\vmsmp\parameters\SwitchList\${switchid}
+
+foreach ( $key in $vmswitch_keys )
+{
+    $vm = $VMSwitchExtensionPortData | where-object {$_.Data -like "*$($key.PSChildName)*"} | select vmname
+    if ( $vm )
+    {
+        echo " $($vm[0].VMName)  $($key.PSChildName) "
+    }
+    else
+    {
+        if ( $($key.PSChildName) )
+        {
+            Remove-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Services\vmsmp\parameters\SwitchList\${switchid}\$($key.PSChildName)" -Recurse
+        }
+    }
+}
+
